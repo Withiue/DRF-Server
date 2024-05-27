c41fe7bfaeb634860e12c03e5e9cee5ee914caaa
Yeonjae Choi <fox2yj@naver.com>
2024-05-28 08:59:38 +0900

    feat: GenericAPIView와 Mixins을 이용한 CRUD 구현

---
+++ blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import Http404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, generics, mixins
+
+from .models import Post
+from .serializers import PostSerializer
+
+class PostListCreateAPIView(mixins.ListModelMixin,
+                            mixins.CreateModelMixin,
+                            generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+class PostRetrieveUpdateDestroyAPIView(mixins.RetrieveModelMixin,
+                                       mixins.UpdateModelMixin,
+                                       mixins.DestroyModelMixin,
+                                       generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+    
+    
