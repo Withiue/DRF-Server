0af5ce3484cd9685270b7bfe5d7e05fe6fec4398
Yeonjae Choi <fox2yj@naver.com>
2024-05-31 20:11:26 +0900

    settings

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'post', PostViewSet)
+
 urlpatterns = [
-    path('', PostListCreateAPIView.as_view(), name='post-list-create'),
-    path('<int:pk>', PostRetrieveUpdateDestroyAPIView.as_view(), name='post-detail'),
+    #CRUD url
+    # path('', PostViewSet.as_view({'get':'list','post':'create'})),
+    # path('<int:pk>', PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update', 'delete':'destroy'})),
+    path('', include(router.urls)),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,37 +3,29 @@ from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, generics, mixins
+from rest_framework import status, generics, mixins, viewsets
 
 from .models import Post
 from .serializers import PostSerializer
 
-class PostListCreateAPIView(mixins.ListModelMixin,
-                            mixins.CreateModelMixin,
-                            generics.GenericAPIView):
+#CRUD 구현하기
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-class PostRetrieveUpdateDestroyAPIView(mixins.RetrieveModelMixin,
-                                       mixins.UpdateModelMixin,
-                                       mixins.DestroyModelMixin,
-                                       generics.GenericAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    def list(self, request, *args, **kwargs):
+        order = request.query_params.get('order')
+        if order == 'user':
+            queryset = self.filter_queryset(self.get_queryset().order_by('user__email'))
+        elif order == 'updated':
+            queryset = self.filter_queryset(self.get_queryset().order_by('-updated_at'))
+        else:
+            queryset = self.filter_queryset(self.get_queryset())
+            
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-    
-    
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
